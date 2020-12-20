94ad2e66e334952200b7a7478f5b06562c29207f
澄鱼 <1067111756@qq.com>
2020-12-21 04:09:28 +0800

    添加 uni-app练习项目

---
+++ study-uni-app/yuedu/src/pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <u-button type="primary">主要按钮</u-button>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+      var loginRes = this.$checkLogin('../index/index', '2')
+      if (!loginRes) return  
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+</style>
