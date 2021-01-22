a82d45f070c3b7eefd32b3ca92adc4b9aa27b5ed
肖潘 <xiaopan@yunjingtech.cn>
2021-01-22 11:58:49 +0800

    数据大屏loading添加渐变色功能

--- study-visual-system/form-report/componentsLib/src/DatavLoading.vue
+++ study-visual-system/form-report/componentsLib/src/DatavLoading.vue
@@ -1,44 +1,63 @@
 /* 大屏加载动画组件 */
 <template>
-  <svg :width="width" :height="height" viewBox="0 0 50 50">
-    <circle 
-      cx="25" 
-      cy="25" 
-      fill="none" 
-      :r="outRadius" 
-      stroke="#409EFF" 
-      stroke-linecap="round"
-      :stroke-dasharray="outStrokeDasharray"
-      :stroke-width="outStrokeWidth"
-    >
-      <animateTransform 
-        attributeName="transform" 
-        type="rotate"
-        values="0 25 25; 360 25 25"
-        :dur="`${outDur}s`"
-        repeatCount="indefinite" 
-      />
-    </circle>
-    <circle 
-      cx="25" 
-      cy="25" 
-      fill="none" 
-      :r="innerRadius" 
-      stroke="#F56C6C" 
-      stroke-linecap="round"
-      :stroke-dasharray="innerStrokeDasharray" 
-      :stroke-width="innerStrokeWidth" 
-    >
-      <animateTransform 
-        attributeName="transform" 
-        type="rotate"
-        values="360 25 25; 0 25 25"
-        :dur="`${innerDur}s`"
-        repeatCount="indefinite" 
-      />
-    </circle>
-  </svg>
-  <slot></slot>
+  <div class="datav-loading__wrapper">
+    <svg :width="width" :height="height" viewBox="0 0 50 50">
+      <circle 
+        cx="25" 
+        cy="25" 
+        fill="none" 
+        :r="outRadius" 
+        stroke="#409EFF" 
+        stroke-linecap="round"
+        :stroke-dasharray="outStrokeDasharray"
+        :stroke-width="outStrokeWidth"
+      >
+        <animateTransform 
+          attributeName="transform" 
+          type="rotate"
+          values="0 25 25; 360 25 25"
+          :dur="`${outDur}s`"
+          repeatCount="indefinite" 
+        />
+
+        <animate
+          v-if="outGradientColor.length > 0"
+          attributeName="stroke" 
+          :values="outGradientColorCmp"
+          :dur="`${outDur}s`"
+          repeatCount="indefinite" 
+        />        
+      </circle>
+      <circle 
+        cx="25" 
+        cy="25" 
+        fill="none" 
+        :r="innerRadius" 
+        stroke="#F56C6C" 
+        stroke-linecap="round"
+        :stroke-dasharray="innerStrokeDasharray" 
+        :stroke-width="innerStrokeWidth" 
+      >
+        <animateTransform 
+          attributeName="transform" 
+          type="rotate"
+          values="360 25 25; 0 25 25"
+          :dur="`${innerDur}s`"
+          repeatCount="indefinite" 
+        />
+
+        <animate
+          v-if="innerGradientColor.length > 0"
+          attributeName="stroke" 
+          :values="innerGradientColorCmp"
+          :dur="`${innerDur}s`"
+          repeatCount="indefinite" 
+        />         
+      </circle>
+    </svg>
+    <slot></slot>
+  </div>
+
 </template>
 
 <script>
@@ -78,11 +97,21 @@ export default {
     innerStrokeWidth: {
       type: Number,
       default: 2
-    }                      
+    },
+    outGradientColor: {
+      type: Array,
+      default: () => []
+    },
+    innerGradientColor: {
+      type: Array,
+      default: () => []
+    }                          
   },
   setup (props) {
     const outRadius = computed(() => 25 - props.outStrokeWidth)  
     const innerRadius = computed(() => Math.round(outRadius.value / 2))  
+    const outGradientColorCmp = computed(() => props.outGradientColor.join(';'))
+    const innerGradientColorCmp = computed(() => props.innerGradientColor.join(';')) 
     const outStrokeDasharray = computed(() => {
       let dash = Math.round(outRadius.value * 2 * 3.14 / 4)
       return `${dash} ${dash}`
@@ -90,14 +119,25 @@ export default {
     const innerStrokeDasharray = computed(() => {
       let dash = Math.round(innerRadius.value * 2 * 3.14 / 4)
       return `${dash} ${dash}`
-    })       
+    })      
 
     return {
       outRadius,
       innerRadius,
       outStrokeDasharray,
-      innerStrokeDasharray
+      innerStrokeDasharray,
+      outGradientColorCmp,
+      innerGradientColorCmp
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .datav-loading__wrapper {
+    display: inline-flex;
+    flex-flow: column;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
