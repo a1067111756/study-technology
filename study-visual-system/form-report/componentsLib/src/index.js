ec491c6cb93dd0bfa8adce3b1600cf9e46bdc08a
澄鱼 <1067111756@qq.com>
2021-02-08 08:20:21 +0800

    some commit

--- study-visual-system/form-report/componentsLib/src/index.js
+++ study-visual-system/form-report/componentsLib/src/index.js
@@ -1,9 +1,11 @@
-import DatavLoading from './DatavLoading.vue'
 import FlyBox from './FlyBox.vue'
+import DatavLoading from './DatavLoading.vue'
+import DatavContainer from './DatavContainer.vue'
 
 export default {
   install: (app, options) => {
     app.component(FlyBox.name, FlyBox)
     app.component(DatavLoading.name, DatavLoading)
+    app.component(DatavContainer.name, DatavContainer)
   }
 }
