3a89879dd636ac9bef0290c4685fac47031fdd9b
澄鱼 <1067111756@qq.com>
2021-06-16 01:01:57 +0800

    初始化nest项目

---
+++ study-nest/mishop-back/src/main.ts
@@ -0,0 +1,33 @@
+import { join } from 'path';
+import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  // 注册应用
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // 配置静态资源目录
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  // 配置cookie
+  app.use(cookieParser('this signed cookies'));
+
+  // 配置session
+  app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: true,
+      saveUninitialized: true,
+      cookie: { maxAge: 1000 * 60 * 30, httpOnly: true },
+      rolling: true,
+    }),
+  );
+
+  // 启动应用
+  await app.listen(3000);
+}
+bootstrap();
