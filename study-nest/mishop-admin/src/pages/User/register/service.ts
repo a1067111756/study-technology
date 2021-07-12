d2b5aab1a87f0b297f2b4b702ca8a676aeab29f9
澄鱼 <1067111756@qq.com>
2021-07-13 01:05:01 +0800

    升级antd-pro到v5

--- study-nest/mishop-admin/src/pages/User/register/service.ts
+++
@@ -1,9 +0,0 @@
-import request from 'umi-request';
-import type { UserRegisterParams } from './index';
-
-export async function fakeRegister(params: UserRegisterParams) {
-  return request('/mock-api/auth/register', {
-    method: 'POST',
-    data: params,
-  });
-}
